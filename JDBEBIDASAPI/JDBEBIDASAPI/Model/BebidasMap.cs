08c62489fffcc51436d4209028bb07d9f3ada8b7
XDfelipin <105940858+XDfelipin@users.noreply.github.com>
2022-11-03 01:02:09 -0300

    1.0

---
+++ JDBEBIDASAPI/JDBEBIDASAPI/Model/BebidasMap.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace JDBEBIDASAPI.Model
+{
+    public class BebidasMap : DbContext
+    {
+        public BebidasMap(DbContextOptions<BebidasMap> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Bebida> Bebidas { get; set; }
+    }
+}
